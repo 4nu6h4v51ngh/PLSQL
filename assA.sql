ce7ae3dbbff4f34acbf366c66b031ef623e952e5
Anubhav Singh <66737188+4nu6h4v51ngh@users.noreply.github.com>
2020-09-17 21:14:45 +0530

    Add files via upload

---
+++ assA.sql
@@ -0,0 +1,3 @@
+create table countries(country_name varchar(3),country_code int,currency_code int);
+insert into countries(country_name,country_code,currency_code)
+values('a',10,001),('b',11,001),('c',12,001),('d',13,001),('e',14,001),('f',15,001);
