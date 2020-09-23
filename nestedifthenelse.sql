ce3afe7c1297ff5b046ce08c910acdd0fd3d2f49
Anubhav Singh <66737188+4nu6h4v51ngh@users.noreply.github.com>
2020-09-23 17:07:16 +0530

    Add files via upload

---
+++ nestedifthenelse.sql
@@ -0,0 +1,15 @@
+DECLARE 
+a number(3) := 100; 
+b number(3) := 200; 
+BEGIN 
+IF( a = 100 ) 
+THEN
+IF( b = 200 ) 
+THEN
+dbms_output.put_line('Value of a is 100 and b is 200' ); 
+END IF; 
+END IF; 
+dbms_output.put_line('Exact value of a is : ' || a ); 
+dbms_output.put_line('Exact value of b is : ' || b ); 
+END; 
+/
