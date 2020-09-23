8f017321d8eda8a42c14b454f62118ef0e2c2828
Anubhav Singh <66737188+4nu6h4v51ngh@users.noreply.github.com>
2020-09-23 17:03:39 +0530

    Add files via upload

---
+++ likeopr.sql
@@ -0,0 +1,15 @@
+DECLARE 
+PROCEDURE compare (value varchar2, pattern varchar2 ) is 
+BEGIN 
+IF value LIKE pattern 
+THEN 
+dbms_output.put_line ('True'); 
+ELSE 
+dbms_output.put_line ('False'); 
+END IF; 
+END; 
+BEGIN 
+compare('Zara Ali', 'Z%A_i'); 
+compare('Nuha Ali', 'Z%A_i'); 
+END;
+/
