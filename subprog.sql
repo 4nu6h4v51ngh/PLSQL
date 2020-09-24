28b057a09c9c1b16259a277f86761af9fadf22fe
Anubhav Singh <66737188+4nu6h4v51ngh@users.noreply.github.com>
2020-09-24 15:18:52 +0530

    Add files via upload

---
+++ subprog.sql
@@ -0,0 +1,28 @@
+DECLARE 
+type books is record 
+(title varchar(50), 
+author varchar(50), 
+subject varchar(100), 
+book_id number); 
+book1 books; 
+book2 books; 
+PROCEDURE printbook (book books) IS 
+BEGIN 
+dbms_output.put_line ('Book title : ' || book.title); 
+dbms_output.put_line('Book author : ' || book.author); 
+dbms_output.put_line( 'Book subject : ' || book.subject); 
+dbms_output.put_line( 'Book book_id : ' || book.book_id); 
+END; 
+BEGIN 
+book1.title := 'C Programming'; 
+book1.author := 'Nuha Ali '; 
+book1.subject := 'C Programming Tutorial'; 
+book1.book_id := 6495407;
+book2.title := 'Telecom Billing'; 
+book2.author := 'Zara Ali'; 
+book2.subject := 'Telecom Billing Tutorial'; 
+book2.book_id := 6495700; 
+printbook(book1);
+printbook(book2); 
+END; 
+/
